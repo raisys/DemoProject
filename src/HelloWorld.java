69d7a8fb3dc9b60f5cba16ccc0fce0fbc556913d
vikash.k.rai <vikash.k.rai@pwc.com>
2020-03-22 17:59:48 -0500

    modularize the code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,14 +4,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, IOWA");
-		System.out.println("Hello, Des Moine");
-		System.out.println("Hello, West Des Moine");		
-		System.out.println("Hello, Waukee");
         System.out.println("Hello, World");
-        System.out.println("Hello, ILLINOIS");
-        System.out.println("Hello, IOWA");
-		System.out.println("Hello, Des Moine");
+    }
+	
+	public static void printHello(String strPlace) {
+        System.out.println("Hello, "+ strPlace);
     }
 
 }
