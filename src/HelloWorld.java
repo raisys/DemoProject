32e2a96fbd56bd8131fdc14b8bd018f0e2bf4e41
vikash.k.rai <vikash.k.rai@pwc.com>
2020-03-22 18:10:47 -0500

    no message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,12 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        System.out.println("Hello, ILLINOIS");
-        System.out.println("Hello, IOWA");
-		System.out.println("Hello, Des Moine");
-		System.out.println("Hello, West Des Moine");		
-		System.out.println("Hello, Waukee");
-        System.out.println("Hello, Iowa");
     }
 	
 	public static void printHello(String strPlace) {
