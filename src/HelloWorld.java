87f88afc5ac04aa966af66978aaf0fc78eec429b
vikash.k.rai <vikash.k.rai@pwc.com>
2020-03-22 18:23:17 -0500

    delete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,6 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
 	printHello("IOWA-US101");
-	printHello ("IILINOIS-US201")   ; 
     }
 	
 	public static void printHello(String strPlace) {
