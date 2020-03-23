86453e2d93f9b35e9b3b70cf3aad1f3e84d4161d
vikash.k.rai <vikash.k.rai@pwc.com>
2020-03-23 05:38:24 -0500

    Revert "Updated code with US 503"
    This reverts commit 1a5f85b8dbe4cad8480ea157cd53cbf84915a306.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,6 @@ public class HelloWorld {
 	printHello("IOWA-US102");
 	printHello("IL-US501");
 	printHello("IL-US502");
-	printHello("IL-US503");
     }
 	
 	public static void printHello(String strPlace) {
